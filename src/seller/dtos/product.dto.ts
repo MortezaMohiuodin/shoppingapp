d6855071c69026c403b07a4746ca5c488590f769
Morteza Mohiuodin <mortezamohiuodin@gmail.com>
2024-06-24 13:52:32 +0330

    create product method  product.service

---
+++ src/seller/dtos/product.dto.ts
@@ -0,0 +1,7 @@
+import {Request} from 'express'
+export interface CreateProductDto{
+    title: string;
+    price:number;
+    userId : string;
+    files: Request['files'] 
+}
---
+++ src/seller/product/product.service.ts
@@ -0,0 +1,41 @@
+import { ProductModel } from "@shp_ahmad5five/common";
+import { Product } from "./product.model";
+import { CreateProductDto } from "../dtos/product.dto";
+import fs from 'fs'
+import path from 'path'
+export class ProductService {
+    constructor(public productModel:ProductModel){
+
+    }
+    async create(createProductDto:CreateProductDto){
+        const images = this.generateProductImages(createProductDto.files)
+        const product = new this.productModel({
+            title : createProductDto.title,
+            price: createProductDto.price,
+            user : createProductDto.userId,
+            images : images
+        })
+        return await product.save()
+    }
+    generateBase64Url(contentType:string,buffer:Buffer){
+        return `data:${contentType}:base64,${buffer.toString('base64')}`
+    }
+    generateProductImages(files:CreateProductDto['files']) : Array<{src:string}>{
+        let images : Array<Express.Multer.File>
+        if(typeof files === 'object'){
+            images = Object.values(files).flat()
+        }else{
+            images = files ? [...files] : []
+        }
+        return images.map((file:Express.Multer.File)=>{
+            let srcObj = {src:this.generateBase64Url(file.mimetype,file.buffer)}
+            fs.unlink(path.join('upload/' + file.filename),()=>{
+
+            })
+            return srcObj
+        })
+
+    }
+
+}
+export const productService = new ProductService(Product)
