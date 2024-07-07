6701838ef022ed7a1b0241da678f2f7326a5ce75
Morteza Mohiuodin <mortezamohiuodin@gmail.com>
2024-07-07 15:29:42 +0330

    create cartadd router  buyer.routers

--- src/buyer/buyer.routers.ts
+++ src/buyer/buyer.routers.ts
@@ -1,5 +1,13 @@
 import {Router,Response,Request,NextFunction} from 'express'
-import {requireAuth} from '@shp_ahmad5five/common'
+import {requireAuth , CustomError} from '@shp_ahmad5five/common'
+import { buyerService } from './buyer.service'
 
 
 const router = Router()
+router.post('/cart/add',requireAuth,async (req:Request,res:Response,next:NextFunction)=>{
+    const {productId,quantity} = req.body
+    const result = await buyerService.addProductToCart({productId,quantity,userId:req.currentUser!.userId})
+    if(result instanceof CustomError || result instanceof Error) return next(result)
+    res.status(200).send(result)
+})
+export {router as buyerRouters}
