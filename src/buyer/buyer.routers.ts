cf3820f26ae1f5b1feb187f82ac493e8c25a82e2
Morteza Mohiuodin <mortezamohiuodin@gmail.com>
2024-07-07 16:07:02 +0330

    update cartProduct quantity router  buyer.router

--- src/buyer/buyer.routers.ts
+++ src/buyer/buyer.routers.ts
@@ -1,5 +1,5 @@
 import {Router,Response,Request,NextFunction} from 'express'
-import {requireAuth , CustomError} from '@shp_ahmad5five/common'
+import {requireAuth , CustomError, BadRequestError} from '@shp_ahmad5five/common'
 import { buyerService } from './buyer.service'
 
 
@@ -10,4 +10,17 @@ router.post('/cart/add',requireAuth,async (req:Request,res:Response,next:NextFun
     if(result instanceof CustomError || result instanceof Error) return next(result)
     res.status(200).send(result)
 })
+
+router.post('/cart/:cartId/product/:id/update-quantity',async (req:Request,res:Response,next:NextFunction)=>{
+    const {amount} = req.body
+    const {cartId,id:productId} = req.params
+    const inc = req.body.inc === 'true' ? true : req.body.inc === 'false' ? false : null
+    if(inc === null) return next(new BadRequestError('inc should be either true or false'))
+    
+    const result = await buyerService.updateCartProductQuantity({cartId,productId,options:{amount}})
+    if(result instanceof CustomError || result instanceof Error) return next(result)
+    res.status(200).send(result)
+})
+
+
 export {router as buyerRouters}
